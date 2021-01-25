a644caf2cae770937558bfc5a5fbcb477f3636e4
kaizhu94 <zhukai0803@gmail.com>
2021-01-24 21:04:07 -0500

    Fix Signup Error Handle

--- frontend/actions/session_action.js
+++ frontend/actions/session_action.js
@@ -1,8 +1,8 @@
 import * as APIUtil from '../util/session_api_util'
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS'
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const receiveCurrentUser = user => {
     return {
@@ -20,6 +20,7 @@ export const receiveErrors = errors => {
         type: RECEIVE_SESSION_ERRORS,
         errors
 }}
+
 
 // export const login = (user) => dispatch => {
 //     return APIUtil.login(user).then(
